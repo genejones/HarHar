3b4e29a0ae241972f37b2a3e3b2ae907787fa05f
Gene Jones <gjones@keystonesolutions.com>
2013-06-05 13:51:08 -0400

    init commit
    HarHar is a python script to understand HTTP Archive Repositories (.HAR)

---
+++ HarHar.py
@@ -0,0 +1,91 @@
+import json
+
+class GlobalComment:
+	self.comment = self.getComment()
+
+	def getComment (self):
+		if 'comment' in self.data:
+			return self.data['comment']
+		else:
+			return None
+
+
+class HAR(GlobalComment):
+	def __init__(self, inputFile):
+		#eventually add args / kwargs for file/string input
+		inputFile.open()
+		interpreted_dict = json.load(inputFile)
+		inputFile.close()
+		self.data = interpreted_dict['log']
+		self.creator = self.getCreator()
+		self.browser = self.getBrowser()
+		self.version = self.getVersion()
+		if 'entries' in self.data:
+			self.entries = []
+			for item in self.data['entries']:
+				self.entries.append(Entry(item))
+		else:
+			self.entries = None
+
+		if 'pages' in self.data:
+			self.pages = []
+			for item in self.data['pages']:
+				self.pages.append(Page(item))
+		else:
+			self.pages = None #pages are optional, so this is 
+
+
+	def getBrowser (self): #browser is optional, return null if not present
+		try:
+			return self.data['browser']
+		except:
+			return None
+
+	def getCreator (self):
+		try:
+			return self.data['creator']
+		except:
+			return None
+
+	def getVersion (self):
+		try:
+			version = self.data['version']
+			if not len(version) > 0:
+				raise TypeError
+			else:
+				return version
+		except:
+			return "1.1" #1.1 is assumed by default, as per the .HAR file spec
+
+	def getMatchedEntries (self, matchString):
+		"""
+		For a given matchString that varies by vendor (e.g. /b/ss for Omniture) yield back request objects and time requests
+		
+		Disregard all entries that aren't HTTP Status == 200, because those aren't real, at least for Omniture.
+		"""
+		entries = self.data['log']['entries']
+		for entry in entries:
+			request = entry['request']
+			response = entry['response']
+			startTime = entry['startedDateTime'] #in ISO 8601
+			if matchString in request['url']: # and (response['status'] is 200 or response['status'] is 302): #look for the matchstring, also check that the status is OK
+				#import pdb;pdb.set_trace()
+				yield request, startTime
+				
+			
+	def getDictFromListofDicts (self, request):
+		stuff = {}
+		for item in request['headers']:
+			stuff[item['name']] = item['value']
+		return stuff
+
+
+class Entry(GlobalComment):
+	#do something
+	def __init__(self, data):
+		if 
+
+
+
+class Page(GlobalComment):
+	#do something
