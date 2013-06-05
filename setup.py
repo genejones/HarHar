e0f708407a1a17f0910a0cc0e69970012e648585
Gene Jones <gjones@keystonesolutions.com>
2013-06-05 14:10:02 -0400

    packaging

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+setup(name='HarHar',
+      version='0.1',
+      description='For Reading .HAR (HTTP Archive Repositories)',
+      author='Gene Jones',
+      maintainer='Gene Jones',
+      url='http://www.genejon.es',
+      py_modules=['HarHar'],
+      )
